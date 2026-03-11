92693c9e2b62c72be73de14eb8f2956fd63879e7
Marchand <baptistemarchand2007@gmail.com>
2026-03-11 14:42:09 +0100

    ajout connexion

--- source/functions/user/user.php
+++ source/functions/user/user.php
@@ -3,8 +3,8 @@
 function setUser($db, $first_name, $last_name, $email, $username, $password)
 {
     var_dump($first_name, $last_name, $email, $username, $password);
-    $sql = "INSERT INTO `staff` (`first_name`,`last_name`,`email`,`username`,`password`)
-    VALUES (:first_name, :last_name, :email, :username, :password)";
+    $sql = "INSERT INTO `staff` (`first_name`,`last_name`,`address_id`,`email`,`store_id`,`username`,`password`)
+    VALUES (:first_name, :last_name,2, :email, 2, :username, :password)";
     $stmt = $db->prepare($sql);
     $stmt->bindValue(':first_name', $first_name, PDO::PARAM_STR);
     $stmt->bindValue(':last_name', $last_name, PDO::PARAM_STR);
@@ -17,7 +17,7 @@ function setUser($db, $first_name, $last_name, $email, $username, $password)
 function getUser($db, $username, $password) {
     $sql = "SELECT `first_name`, `last_name`
     FROM `staff`
-    WHERE `first_name` = :first_name AND `password` = :password";
+    WHERE `username` = :username AND `password` = :password";
     $stmt = $db->prepare($sql);
     $stmt->bindValue(':username', $username, PDO::PARAM_STR);
     $stmt->bindValue(':password', $password, PDO::PARAM_STR);
--- source/pages/connexion.php
+++ source/pages/connexion.php
@@ -1,5 +1,7 @@
 <?php include_once('../partials/header.php');
 include_once __DIR__ . '/../functions/user/user.php';
+$singIn = false;
+$logIn = false;
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $db = db();
     if (isset($_POST['signIn'])) {
@@ -18,94 +20,102 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // champ invalide ou manquant
             die("Données invalides");
         }
+        // faire des regex avec pregmatch
         // crypte le mot de passe 
         $password = password_hash($password, PASSWORD_BCRYPT);
-        // faire des regex
         $user = setUser($db, $first_name, $last_name, $email, $username, $password);
         if ($user === true) {
-            var_dump('Vous êtes bien inscrit');
+            $singIn = true;
         }
     }
     if (isset($_POST['logIn'])) {
         $username = $_POST['username'];
         $password = $_POST['password'];
         $password = password_hash($password, PASSWORD_BCRYPT);
-
-        getUser($db, $username, $password);
+        $log = getUser($db, $username, $password);
+        if ($log === true) {
+            $logIn = true;
+        }
     }
 }
 
 ?>
 <p><span>Consigne : </span>faut créé un compt staff et ce connecter avec et savoir si on est connecter</p>
 <p>faut mettre l'identifiant de l'adress en dure pareil pour le store id</p>
+<?php if ($singIn === true) { ?>
+    <p>Vous êtes bien inscrit</p>
+<?php } ?>
 <div class="divLog">
     <section class="log">
         <h3>Ici vous pouvez vous Inscrire</h3>
         <form action="" method="POST">
             <div>
                 <label for="first_name">Prénom : </label>
-            <input
-                type="text"
-                id="first_name"
-                name="first_name"
-                required />
+                <input
+                    type="text"
+                    id="first_name"
+                    name="first_name"
+                    required />
             </div>
             <div>
                 <label for="last_name">Nom : </label>
                 <input
-                type="text"
-                id="last_name"
-                name="last_name"
-                required />
+                    type="text"
+                    id="last_name"
+                    name="last_name"
+                    required />
             </div>
             <div>
                 <label for="email">Email : </label>
                 <input
-                type="email"
-                id="email"
-                name="email"
-                required />
+                    type="email"
+                    id="email"
+                    name="email"
+                    required />
             </div>
             <div>
                 <label for="username">Pseudo : </label>
                 <input
-                type="text"
-                id="username"
-                name="username"
-                required />
+                    type="text"
+                    id="username"
+                    name="username"
+                    required />
             </div>
             <div>
                 <label for="password">Mot de passe : </label>
                 <input
-                type="password"
-                id="password"
-                name="password"
-                required />
+                    type="password"
+                    id="password"
+                    name="password"
+                    required />
             </div>
             <button type="submit" name="signIn">S'inscrire</button>
         </form>
     </section>
     <section class="log">
+        <?php if ($logIn === true) { ?>
+            <p>Vous êtes bien connecter</p>
+        <?php } ?>
         <h3>Ici vous pouvez vous connecter</h3>
         <form action="" method="POST">
             <div>
                 <label for="username">Pseudo : </label>
                 <input
-                type="text"
-                id="username"
-                name="username"
-                required />
+                    type="text"
+                    id="username"
+                    name="username"
+                    required />
             </div>
-        <div>
-            <label for="password">Mot de passe : </label>
-            <input
-            type="password"
-            id="password"
-            name="password"
-            required />
-        </div>
-        <button type="submit" name="logIn">Connexion</button>
-    </form>
-</section>
+            <div>
+                <label for="password">Mot de passe : </label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    required />
+            </div>
+            <button type="submit" name="logIn">Connexion</button>
+        </form>
+    </section>
 </div>
-<?php include_once('../partials/footer.php');
+<?php include_once('../partials/footer.php'); ?>
